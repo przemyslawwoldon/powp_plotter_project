2b8095c5e9358e97eee6e3469679d86a15030495
PrzemyslawWoldon <przemyslawwoldon@gmail.com>
2017-04-20 14:48:50 +0200

    3.1.1 Stworzenie hierarchi

---
+++ src/main/java/edu/iis/powp/hierarchy/CommandDrawLineToPosition.java
@@ -0,0 +1,14 @@
+package edu.iis.powp.hierarchy;
+
+public class CommandDrawLineToPosition implements PlotterCommand{
+
+	private int x;
+	private int y;
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
