47444120630ff623da10e7ff9eeecb1b566fc315
PrzemyslawWoldon <przemyslawwoldon@gmail.com>
2017-04-29 17:36:20 +0200

    Temp

--- src/main/java/edu/iis/powp/factory/FigureFactory.java
+++ src/main/java/edu/iis/powp/factory/FigureFactory.java
@@ -1,6 +1,17 @@
 package edu.iis.powp.factory;
 
-public interface FigureFactory {
+public class FigureFactory implements IFigureFactory {
 
-	
+	@Override
+	public Square getSquare() {
+		// TODO Auto-generated method stub
+		return new Square();
+	}
+
+	@Override
+	public Circle getCircle() {
+		// TODO Auto-generated method stub
+		return new Circle();
+	}
+
 }
---
+++ src/main/java/edu/iis/powp/factory/IFigureFactory.java
@@ -0,0 +1,9 @@
+package edu.iis.powp.factory;
+
+public interface IFigureFactory {
+
+	public Square getSquare();
+	
+	public Circle getCircle();
+	
+}
